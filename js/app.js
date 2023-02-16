4ba2160fd1e0619e80aad9065691e240a0edae26
antik1801 <antik.edu@gmail.com>
2023-02-17 00:35:26 +0600

    edited

--- js/app.js
+++ js/app.js
@@ -1,11 +1,40 @@
-function getPin(){
+function getPin() {
     const pin = generatePin();
     const pinString = pin + '';
-    console.log(pin,typeof pin)
-    console.log(pinString,typeof pinString);
-    return 0;
+    if (pinString.length === 5) {
+        return pinString;
+    }
+    else {
+        return getPin();
+    }
 }
-function generatePin(){
-    const random = Math.round(Math.random()*100000);
+function generatePin() {
+    const random = Math.round(Math.random() * 100000);
     return random;
 }
+document.getElementById('genate-pin').addEventListener('click', function () {
+    const pin = getPin();
+    const displayPinField = document.getElementById('display-pin');
+    displayPinField.value = pin;
+})
+document.getElementById('calculator').addEventListener('click', function (event) {
+    const number = event.target.innerText;
+    const typedNumberField = document.getElementById('typed-number');
+    const previousTypedNumber = typedNumberField.value;
+    if(isNaN(number)){
+        if(number === 'C'){
+            typedNumberField.value = '';
+        }
+        else if(number === '<'){
+          const digits = previousTypedNumber.split('');
+          digits.pop();
+          const joinDigits = digits.join("");
+          typedNumberField.value=joinDigits;
+          
+        }
+    }
+    else{
+        const newTypedNumber = previousTypedNumber + number;
+        typedNumberField.value=newTypedNumber;
+    }
+})
